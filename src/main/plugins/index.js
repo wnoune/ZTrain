ba1926a8eda44d5eee8dda16765451952c22fb2d
wnoune <wilfriednoune3@gmail.com>
2022-03-11 11:32:40 +0100

    Ztrain_OK

--- src/main/plugins/index.js
+++ src/main/plugins/index.js
@@ -13,10 +13,17 @@
 // the project's config changing)
 
 /**
- * @type {Cypress.PluginConfig}
+
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+//module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  const cucumber = require('cypress-cucumber-preprocessor').default
+
+  module.exports = (on, config) => {
+    on('file:preprocessor', cucumber());
 }
+
+
